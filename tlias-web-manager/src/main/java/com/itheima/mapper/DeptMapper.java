59569469d379bdd8b39d3287cacd8dab41854c34
amadeus <1685736247@qq.com>
2025-12-22 22:54:39 +0800

    feat: 结果封装
    1.Result映射
    2.sql语句起别名
    3.配置文件启动驼峰

--- tlias-web-manager/src/main/java/com/itheima/mapper/DeptMapper.java
+++ tlias-web-manager/src/main/java/com/itheima/mapper/DeptMapper.java
@@ -8,7 +8,9 @@ import java.util.List;
 
 @Mapper
 public interface DeptMapper {
-
+    //方法1:Result映射
+    //方法2:select起别名
+    //方法3:配置文件中打开mybatis驼峰转换
     @Select("select * from dept order by update_time desc")
     public List<Dept> findAll();
 }
