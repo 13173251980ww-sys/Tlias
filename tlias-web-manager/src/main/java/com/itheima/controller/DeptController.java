573e06d113fc64193adfb728ca653ac9650b83d4
amadeus <1685736247@qq.com>
2025-12-23 09:24:06 +0800

    feat: 删除部门Controller层接收参数的三种方法
    方法一：HttpServletRequest
    方法二: @RequestParam注解
    方法三：若名字相同，则@RequestParam注解可以省略

--- tlias-web-manager/src/main/java/com/itheima/controller/DeptController.java
+++ tlias-web-manager/src/main/java/com/itheima/controller/DeptController.java
@@ -3,13 +3,15 @@ package com.itheima.controller;
 import com.itheima.pojo.Dept;
 import com.itheima.pojo.Result;
 import com.itheima.service.DeptService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpRequest;
 import java.util.List;
 
 @RestController
@@ -17,9 +19,32 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
-    @RequestMapping("/depts")
-    public Result deptController(){
+    @GetMapping("/depts")
+    public Result get(){
          List<Dept> deptList=deptService.findAll();
          return Result.success(deptList);
     }
+
+//    @DeleteMapping("/depts")
+//    public Result delete(HttpServletRequest request){
+//        String params =request.getParameter("id");
+//        int id=Integer.parseInt(params);
+//        System.out.println("删除id为"+id+"的部门");
+//        return Result.success();
+//    }
+
+//    方式二：通过RequestParam注解来实现
+//    @DeleteMapping("/depts")
+//    public Result delete(@RequestParam("id") Integer id){
+//        System.out.println("删除id为"+id+"的部门");
+//        return Result.success();
+//    }
+
+    //方式三：若名字相同，则@Params可以省略
+    @DeleteMapping("/depts")
+    public Result delete(Integer id){
+        System.out.println("删除id为"+id+"的部门");
+        return Result.success();
+    }
+
 }
